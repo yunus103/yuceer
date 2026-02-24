b2d7ed0de28c57c7570c120de76c0ab28a2f1d77
yunus103 <yunusemre.aytekin@ogr.iuc.edu.tr>
2026-02-24 21:22:41 +0300

    Logo size increased

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -40,13 +40,13 @@ export default function Navbar({ logo, phone, phone2, email }: NavbarProps) {
       <header
         className={cn(
           'fixed top-0 left-0 right-0 z-50 transition-all duration-300 border-b',
-          showBackground ? 'bg-dark-bg shadow-md py-2 md:py-3 border-white/5 opacity-99' : 'bg-transparent py-4 md:py-6 border-transparent'
+          showBackground ? 'bg-dark-bg shadow-md py-1 md:py-1.5 border-white/5 opacity-99' : 'bg-transparent py-2 md:py-4 border-transparent'
         )}
       >
         <div className="container mx-auto px-4 flex items-center justify-between">
           <Link href="/" className="flex items-center gap-1 z-50 group">
               {logo ? (
-                <div className="relative h-20 w-52">
+                <div className="relative h-20 w-60 md:h-24 md:w-68 transition-all duration-300">
                    <Image 
                       src={logo} 
                       alt="Yüceer Logo" 
