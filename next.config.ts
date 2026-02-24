9ffe8a791a2ad635f799f95b607cb92fb2d35222
yunus103 <yunusemre.aytekin@ogr.iuc.edu.tr>
2026-02-24 20:31:31 +0300

    Config

--- next.config.ts
+++ next.config.ts
@@ -2,19 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    qualities: [60, 75, 85, 90, 100],
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
+        protocol: "https",
+        hostname: "images.unsplash.com",
       },
       {
-        protocol: 'https',
-        hostname: 'cdn.sanity.io',
+        protocol: "https",
+        hostname: "cdn.sanity.io",
       },
       {
-        protocol: 'https',
-        hostname: 'via.placeholder.com',
-      }
+        protocol: "https",
+        hostname: "via.placeholder.com",
+      },
     ],
   },
 };
